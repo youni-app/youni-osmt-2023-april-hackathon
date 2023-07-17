1cf974f987b8132deecd35bfe2f13adf0035b18e
geepara <62688021+geepara@users.noreply.github.com>
2023-07-17 21:54:14 +0300

    changes

--- upload.py
+++ upload.py
@@ -26,5 +26,5 @@ async def main() -> None:
     except Exception as e: print(e)
 
 if __name__ == '__main__':
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
